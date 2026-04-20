c539e45d74b1a1c62be872a48b80747e7617b776
sebastian <sebaalemunioz@gmail.com>
2026-04-20 16:16:08 -0300

    agrege comentarios al ej 1

--- 2026-1C/clase-03/ejemplo01.c
+++ 2026-1C/clase-03/ejemplo01.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+//Int argc
+//Es un número entero. Te dice cuántas palabras escribiste en la terminal para ejecutar el programa (incluyendo el nombre del programa mismo).
+
+//char *argv[]
+//Es un arreglo de cadenas de caracteres (strings). Cada elemento del arreglo es una palabra que escribiste en la terminal. El primer elemento (argv[0]) es el nombre del programa, y los siguientes elementos (argv[1], argv[2], etc.) son las palabras adicionales que escribiste.
 int main(int argc, char *argv[]) {
     printf("Numero de argumentos (argc): %d\n", argc);
 
