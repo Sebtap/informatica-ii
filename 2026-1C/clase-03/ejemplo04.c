e3aa4273cd60b82991fc7afbc6022dab0d5ac0f1
sebastian <sebaalemunioz@gmail.com>
2026-04-27 13:29:52 -0300

    ag comentarios

--- 2026-1C/clase-03/ejemplo04.c
+++ 2026-1C/clase-03/ejemplo04.c
@@ -7,8 +7,8 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    char *fin;
-    long valor = strtol(argv[1], &fin, 10);
+    char *fin; //es una variable de tipo puntero. su trabajo es guardar una direccion de memoria.
+    long valor = strtol(argv[1], &fin, 10); //&fin Strtol escribe una direccion de memoria del primer caracter que no pudo procesar
 
     if (*fin != '\0') {
         printf("El texto '%s' no es un numero valido.\n", argv[1]);
